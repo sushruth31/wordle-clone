660f788f8c2b45c5c2aeef44bab8060f3a63bc3f
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-04-24 20:51:20 -0400

    lot of functionality added

--- src/grid.js
+++ src/grid.js
@@ -4,42 +4,83 @@ import Keyboard from "./keyboard";
 import useKeyInput from "./useKeyInput";
 
 let wordOfTheDay = "hello";
+//my word is 'haehjl'
 
 const getKey = (rowI, cellI) => `${rowI} ${cellI}`;
+
+function findIndexes(ltr, ltrI) {
+  let indexes = [];
+  let compareWord = [...wordOfTheDay].map(el => el.toUpperCase()).join("");
+  for (let i = 0; i < compareWord.length; i++) {
+    const wordOfTheDayLtr = compareWord[i];
+    if (wordOfTheDayLtr === ltr) {
+      let perfectMatch = false;
+      if (ltrI === i) {
+        perfectMatch = true;
+      }
+      indexes.push({ index: i, perfectMatch });
+    }
+  }
+  return indexes;
+}
 
 export default function Grid() {
   const [currentRow, setCurrentRow] = useState(0);
   const [currentSquareinRow, setCurrentSqaureInRow] = useState(-1);
   const [gridMap, setGridMap] = useState(new Map());
-  const [keyEvent, setKey] = useState(null);
   const [attempts, setAttempts] = useState(new Map());
+  const [keyEvent, setKey] = useState(null);
+  const [isWinner, setIsWinner] = useState(false);
 
   //gridmap: "0 1" => 'k'
 
-  //attemptsMap: 0 => [{letter: 'b', isInWord: true; correctIndex: true}, {}]
+  //attemptsMap: 0 => [{letter: 'b', index: [0, 1] }, {}]
 
-  const checkIfMatch = enteredWord => {
+  function addToAttempts(enteredWord) {
     //add the attempt to the attempts map
 
-    const isLetterInWord = () => {};
-    const isCorrectIndex = () => {};
+    let map = new Map();
 
     //create array of objects value for map
     let val = [];
-    for (let i = 0; i < enteredWord; i++) {
-      let letter = enteredWord[i];
+    for (let i = 0; i < enteredWord.length; i++) {
+      let letter = enteredWord[i].toUpperCase();
+
       val.push({
         letter,
-        isInWord: isLetterInWord(),
-        correctIndex: isCorrectIndex(),
+        indexes: findIndexes(letter, i),
       });
     }
+    function isWinner(attempt) {
+      //first check if any index prop has length of 0
 
-    setAttempts(attempts => {
-      let attemptsCopy = new Map([...attempts]);
-      attemptsCopy.set(currentRow.val);
-    });
-  };
+      if (attempt.some(o => !o.indexes.length)) {
+        return false;
+      }
+
+      let enteredLetters = attempt
+        .map(o => o.letter)
+        .map(ltr => ltr.toUpperCase())
+        .join("");
+      for (let i = 0; i < enteredLetters.length; i++) {
+        if (enteredLetters[i] !== wordOfTheDay[i].toUpperCase()) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    //check if word is a winner
+
+    if (isWinner(val)) {
+      setIsWinner(true);
+      return console.log("you win!");
+    }
+
+    setAttempts(attempts => new Map(attempts).set(currentRow, val));
+  }
+
+  console.log(attempts);
 
   const handleEnter = () => {
     //first check if we have 5 letters
@@ -56,13 +97,12 @@ export default function Grid() {
 
     if (letters.length !== 5) return;
 
-    //now get the word
-    let enteredWord = letters.join("");
+    //add to attempts
+    addToAttempts(letters);
 
-    checkIfMatch(enteredWord, wordOfTheDay);
-
-    // setCurrentRow(r => r + 1);
-    // setCurrentSqaureInRow(-1);
+    //go to next row and reset pos to 0
+    setCurrentRow(r => r + 1);
+    setCurrentSqaureInRow(-1);
   };
 
   const mapSet = (rowI, cellI, val) => {
@@ -86,9 +126,9 @@ export default function Grid() {
     });
   }, [keyEvent]);
 
-  useEffect(() => {
-    console.log({ gridMap, currentSquareinRow });
-  }, [gridMap]);
+  // useEffect(() => {
+  //   console.log({ gridMap, currentSquareinRow });
+  // }, [gridMap]);
 
   const handleDelete = () => {
     if (currentSquareinRow < 0) return;
@@ -104,16 +144,35 @@ export default function Grid() {
     <>
       {[...Array(6)].map((_, rowI) => (
         <div key={rowI} className="flex">
-          {[...Array(5)].map((_, cellI) => (
-            <div
-              key={cellI}
-              className="border-[1px] m-1 border-gray-500 h-[60px] w-[60px] flex items-center justify-center"
-            >
-              <Typography color="white" variant="h4">
-                {gridMap.get(getKey(rowI, cellI))}
-              </Typography>
-            </div>
-          ))}
+          {[...Array(5)].map((_, cellI) => {
+            let addClass = "";
+            let attempt = attempts.get(rowI)?.[cellI]?.indexes;
+            console.log(attempt);
+            if (attempt?.length > 0) {
+              addClass += "bg-orange-500";
+              //check if perfect match now
+              let perfectMatch = attempt.find(
+                o => o.index === cellI
+              )?.perfectMatch;
+              if (perfectMatch) {
+                addClass = "bg-green-500";
+              }
+            }
+            return (
+              <div
+                key={cellI}
+                className={
+                  "border-[1px] m-1 border-gray-500 h-[60px] w-[60px] flex items-center justify-center" +
+                  " " +
+                  addClass
+                }
+              >
+                <Typography color="white" variant="h4">
+                  {gridMap.get(getKey(rowI, cellI))}
+                </Typography>
+              </div>
+            );
+          })}
         </div>
       ))}
       <Keyboard
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+window.print = (...args) => console.log(...args);
 document.body.style = "background: #131214";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
